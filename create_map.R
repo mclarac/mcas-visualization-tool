aef89fd2d03eedbe602a6ea33486ca4cc32ff588
María Camila Lara <mclarac91@gmail.com>
2021-04-24 22:06:14 -0400

    initial commit

---
+++ create_map.R
@@ -0,0 +1,53 @@
+create_map <- function(map, map_data, by, primary = TRUE, palette = "Blues"){
+    
+    data <- map_data@data
+    
+    n_max <- round(max(data$wt.x), digits = 0) + .5
+    
+    bins <- seq(from = 0, to = n_max, by = n_max / 5)
+    
+    pal <- colorBin(palette, domain = data$wt.x, bins = bins)
+    
+    if(by == "Destination"){
+        if(primary) states_map <- mex_states else states_map <- us_states
+    } else {
+        if(primary) states_map <- us_states else states_map <- mex_states
+    }
+    
+    labs <- paste0(
+        "<b>", data$NAME, " (", data$STATEABBR, ")</b><br>",
+        "w.r.t ", by, ": ",
+        format(data$migrations, nsmall = 0, big.mark = ","), " migrations (", round(data$wt.x, 1), "%)",
+        "<br>",
+        "w.r.t all MCAS: ",
+        format(data$n_total, nsmall = 0, big.mark = ","), " migrations (", round(data$wt.y, 1), "%)"
+    ) %>% as.list()
+    
+    map %>%
+        addPolygons(
+            data = map_data,
+            fillColor = ~ pal(wt.x),
+            label = lapply(labs, HTML),
+            weight = 1,
+            opacity = 1,
+            color = "black",
+            dashArray = "1",
+            fillOpacity = 0.85,
+            layerId = ~ GEOID
+        ) %>%
+        addLegend(
+            data = map_data,
+            pal = pal,
+            values = ~wt.x,
+            opacity = 0.85,
+            position = "bottomright",
+            title = "Migrations Percentages",
+            labFormat = labelFormat(suffix = "%")
+        ) %>%
+        addPolylines(
+            data = states_map,
+            color = "black",
+            opacity = 1,
+            weight = 2
+        )
+}
